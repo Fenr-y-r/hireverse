87f70af4b94adcca7170b913e9bdc13d7a449262
bassel27 <bassel_sabour@hotmail.com>
2024-12-14 20:04:43 +0200

    refactor frame code

--- frame.py
+++ frame.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import cv2
+from typing import Tuple
 
 class Frame:
     def __init__(self, index, participant, image):
@@ -9,24 +10,37 @@ class Frame:
         self.smile_area = None
         self.face = None
         self.smile = None
-
-    def display(self):
-        plt.imshow(cv2.cvtColor(self.image, cv2.COLOR_BGR2RGB))
-        plt.title(f"Frame {self.index}")
-        plt.axis('off')
-        plt.show()
+        self.facial_landmarks = None
+        self.copied_image_for_drawing = None
 
     def __str__(self):
         return f"index={self.index}, smile_area={self.smile_area}, participant={self.participant}"
 
+    def create_drawable_image_copy(self):
+        self.copied_image_for_drawing = self.image.copy()
+
     def draw_smile(self):
         if self.smile is not None:
-            # Draw a rectangle around the smile
-            (x, y, w, h) = self.smile  # Extract coordinates of the smile
-            cv2.rectangle(self.image, (x, y), (x + w, y + h), (0, 255, 0), 2)  # Draw green rectangle
+            self.draw_rectangle(self.smile, (0, 0, 255))
 
     def draw_face(self):
-        # Optionally, draw the face rectangle if face is not None
         if self.face is not None:
-            (fx, fy, fw, fh) = self.face  # Extract face coordinates
-            cv2.rectangle(self.image, (fx, fy), (fx + fw, fy + fh), (255, 0, 0), 2)  # Draw blue rectangle
+            self.draw_rectangle(self.face, (255, 0, 0))
+
+    def draw_facial_landmarks(self):
+        if self.facial_landmarks is not None:
+            for (x, y) in self.facial_landmarks:
+                self.draw_cirle((x,y))
+    
+    def draw_rectangle(self, x_y_w_h_tuple, color):
+        (x, y, w, h) = x_y_w_h_tuple
+        cv2.rectangle(self.copied_image_for_drawing, (x, y), (x + w, y + h), color, 2)
+
+    def draw_cirle(self, coordinates: Tuple[int, int]):
+        cv2.circle(self.copied_image_for_drawing, coordinates, 2, (0, 255, 0), -1)
+
+    def display(self):
+        plt.imshow(cv2.cvtColor(self.copied_image_for_drawing, cv2.COLOR_BGR2RGB))
+        plt.title(f"Frame {self.index}")
+        plt.axis('off')
+        plt.show()
--- smile_detector.py
+++ smile_detector.py
@@ -1,7 +1,28 @@
-import numpy as np
-from frame import Frame
 
 import cv2
+import dlib
+from imutils import face_utils
+
+from frame import Frame
+
+# ### Cascade classifiers
+
+# The “cascade” in cascade classifiers refers to the process where the input image is passed through a series of stages or filters.
+# Each stage uses a weak classifier to determine if the object of interest (e.g., a face or a smile) is present.
+# If the image passes through a stage, it proceeds to the next stage. If it fails a stage, the image is rejected.
+
+# - Haar Features: The classifier uses Haar-like features to detect objects.
+#   They are a set of simple rectangular features used in computer vision, particularly for object detection, such as face detection.
+
+# ### Grayscale
+
+# - Converting an image to grayscale is a common preprocessing step in computer vision tasks. Reasons:
+
+#   1. Simplification: Grayscale has only one channel compared to the three channels (RGB). This makesprocessing faster and more efficient.
+
+#   2. Feature Extraction:computer vision algorithms work better on grayscale images because they rely on intensity changes rather than color changes.
+
+#   3. Noise Reduction
 
 
 class SmileDetector:
@@ -16,7 +37,7 @@ class SmileDetector:
         return w * h
 
     def normalize_smile_area(self, min_smile_area, max_smile_area, smile_area):
-        if(max_smile_area == min_smile_area):
+        if max_smile_area == min_smile_area:
             return smile_area
         normalized_smile_area = (
             (smile_area - min_smile_area) / (max_smile_area - min_smile_area) * 100
@@ -30,7 +51,7 @@ class SmileDetector:
     def convert_to_gray(self, frame_image):
         return cv2.cvtColor(frame_image, cv2.COLOR_BGR2GRAY)
 
-    def get_face(self, frame_image):    
+    def get_face(self, frame_image):
         gray_image = self.convert_to_gray(frame_image)
         face_cascade = cv2.CascadeClassifier(
             cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
@@ -41,6 +62,7 @@ class SmileDetector:
         return max(faces, key=self.calculate_face_area) if len(faces) else None
 
     """This returns the coordinates of one smile relative to the face not the whole frame"""
+
     def get_smile(self, frame_image, largest_face):
         (x, y, w, h) = largest_face
         gray_frame = cv2.cvtColor(frame_image, cv2.COLOR_BGR2GRAY)
@@ -55,10 +77,17 @@ class SmileDetector:
             index = 0
             if len(smiles) > 1:
                 index = 1
-            (sx, sy, sw, sh) = smiles[index]    
+            (sx, sy, sw, sh) = smiles[index]
             return (x + sx, y + sy, sw, sh)
         else:
             return None
-    
-   
-    
+
+    def get_face_landmarks(self, frame: Frame):
+        if frame.face is None:
+            return None
+        (x, y, w, h) = frame.face
+        predictor = dlib.shape_predictor("models/shape_predictor_68_face_landmarks.dat")
+        dlib_rect = dlib.rectangle(int(x), int(y), int(x + w), int(y + h))
+        # uses a facial landmark predictor to detect landmarks within a specified region of the image
+        landmarks = predictor(self.convert_to_gray(frame.image), dlib_rect)
+        return face_utils.shape_to_np(landmarks)  # convert to a NumPy array
