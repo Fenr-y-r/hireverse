b01892dae02e9cb2214981e581b7046d0f8ec821
bassel27 <bassel_sabour@hotmail.com>
2024-12-27 10:39:25 +0200

    add smile_intensity metrics

--- models/selected_facial_landmarks.py
+++ models/selected_facial_landmarks.py
@@ -35,3 +35,25 @@ class SelectedFacialLandmarks:
         self.inner_lip_below = kwargs.get("inner_lip_below", None)
         self.lip_corner_right = kwargs.get("lip_corner_right", None)
         self.lip_corner_left = kwargs.get("lip_corner_left", None)
+
+    def __str__(self):
+        return (
+            f"SelectedFacialLandmarks("
+            f"inner_brow_left={self.inner_brow_left}, "
+            f"outer_brow_left={self.outer_brow_left}, "
+            f"inner_brow_right={self.inner_brow_right}, "
+            f"outer_brow_right={self.outer_brow_right}, "
+            f"eye_outer_left={self.eye_outer_left}, "
+            f"eye_outer_right={self.eye_outer_right}, "
+            f"eye_inner_left={self.eye_inner_left}, "
+            f"eye_inner_right={self.eye_inner_right}, "
+            f"outer_lip_height={self.outer_lip_height}, "
+            f"inner_lip_height={self.inner_lip_height}, "
+            f"lip_corner_distance={self.lip_corner_distance}, "
+            f"outer_lip_above={self.outer_lip_above}, "
+            f"outer_lip_below={self.outer_lip_below}, "
+            f"inner_lip_above={self.inner_lip_above}, "
+            f"inner_lip_below={self.inner_lip_below}, "
+            f"lip_corner_right={self.lip_corner_right}, "
+            f"lip_corner_left={self.lip_corner_left})"
+        )
