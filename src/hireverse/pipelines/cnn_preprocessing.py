8a367609eae786e5011a58ea54c7a427524815f2
bassel27 <bassel_sabour@hotmail.com>
2025-04-29 15:30:27 +0300

    use h5 for storage instead of npz

--- src/hireverse/pipelines/cnn_preprocessing.py
+++ src/hireverse/pipelines/cnn_preprocessing.py
@@ -1,7 +1,9 @@
+import gc
 import random
 import time
 import uuid
 import cv2
+import h5py
 from hireverse.utils.utils import *
 from hireverse.utils.face_analyzer import FaceAnalyzer
 from hireverse.schemas.frame import Frame
@@ -64,12 +66,32 @@ def get_processed_frames_images(vid_file_path: str):
 
 def process_single_video(vid_path, output_dir):
     frame_data = get_processed_frames_images(vid_path)
-    vid_name = Path(vid_path).stem
-    output_path = os.path.join(output_dir, f"{vid_name}.npz")
-    labels = get_labels_dict_from_participant_id(vid_name)
-    np.savez(output_path, frames=frame_data, **labels)
-    print(f"\nDone! Saved {vid_name}")
-    return True
+    participant_id = Path(vid_path).stem
+    output_path = os.path.join(output_dir, f"{participant_id}.h5")
+    labels = get_labels_dict_from_participant_id(participant_id)
+    with h5py.File(output_path, "w") as h5f:
+        # Create a resizable dataset: 0 initial frames, unlimited max
+        dset = h5f.create_dataset(
+            "frames",
+            shape=(0, 640, 640),    # starts with zero frames stored
+            maxshape=(None, 640, 640),  # unlimited frames
+            dtype="float32",
+            chunks=(1, 640, 640),   # 1 frame of size 640*640 at a time 
+            compression="gzip"
+        )
+        # Store labels as attributes in a labels group
+        lbl_grp = h5f.create_group("labels")
+        for k, v in labels.items():
+            lbl_grp.attrs[k] = v
+
+        # Append each frame one by one
+        for idx, img in enumerate(frame_data):
+            dset.resize((idx + 1, 640, 640))    # Each loop increases the dataset length by one
+            # write the new slice
+            dset[idx, :, :] = img
+        del frame_data; gc.collect()    # garbage collector frees memory by removing objects that your program no longer uses
+
+    print(f"Saved {participant_id}")
   
 
 
